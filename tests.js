93bc0482f7c2597fdc8640ea00600544a8295fb4
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-19 13:19:07 +0530

    modified:   tests.js

--- tests.js
+++ tests.js
@@ -359,3 +359,152 @@ QUnit.module("Logo Unit Tests", {
       this.assert_equals('(reverse [ a b c ] "de)', 'cbade');
     
       this.assert_equals('gensym <> gensym', 1);
+      
+  this.assert_equals('first (list 1 2 3 )', 1);
+  this.assert_equals('firsts [ [ 1 2 3 ] [ "a "b "c] ]', ["1", '"a']);
+  this.assert_equals('last [ a b c ]', "c");
+  this.assert_equals('butfirst [ a b c ]', ["b", "c"]);
+  this.assert_equals('butfirst "abc', 'bc');
+  this.assert_equals('bf [ a b c ]', ["b", "c"]);
+  this.assert_equals('butfirsts [ [ 1 2 3 ] [ "a "b "c] ]', [["2", "3"], ['"b', '"c']]);
+  this.assert_equals('butfirsts [ 123 abc ]', ['23', 'bc']);
+  this.assert_equals('bfs [ [ 1 2 3 ] [ "a "b "c] ]', [["2", "3"], ['"b', '"c']]);
+  this.assert_equals('butlast  [ a b c ]', ["a", "b"]);
+  this.assert_equals('bl [ a b c ]', ["a", "b"]);
+
+  this.assert_equals('first "123', '1');
+  this.assert_equals('last  "123', '3');
+  this.assert_equals('first "abc', 'a');
+  this.assert_equals('last  "abc', 'c');
+  this.assert_equals('butfirst "123', '23');
+  this.assert_equals('butlast  "123', '12');
+
+  this.assert_equals('first 123', '1');
+  this.assert_equals('last  123', '3');
+  this.assert_equals('butfirst 123', '23');
+  this.assert_equals('butlast  123', '12');
+
+  this.assert_error('item 0 [ a b c ]', 'ITEM: Index out of bounds');
+  this.assert_equals('item 1 [ a b c ]', "a");
+  this.assert_equals('item 2 [ a b c ]', "b");
+  this.assert_equals('item 3 [ a b c ]', "c");
+  this.assert_error('item 4 [ a b c ]', 'ITEM: Index out of bounds');
+
+  this.assert_error('item 0 { a b c }', 'ITEM: Index out of bounds');
+  this.assert_equals('item 1 { a b c }', "a");
+  this.assert_equals('item 2 { a b c }', "b");
+  this.assert_equals('item 3 { a b c }', "c");
+  this.assert_error('item 4 { a b c }', 'ITEM: Index out of bounds');
+
+  this.assert_equals('item 0 { a b c }@0', 'a');
+  this.assert_equals('item 1 { a b c }@0', 'b');
+  this.assert_equals('item 2 { a b c }@0', 'c');
+  this.assert_error('item 3 { a b c }@0', 'ITEM: Index out of bounds');
+
+  this.assert_error('item 0 "abc', 'ITEM: Index out of bounds');
+  this.assert_equals('item 1 "abc', "a");
+  this.assert_equals('item 2 "abc', "b");
+  this.assert_equals('item 3 "abc', "c");
+  this.assert_error('item 4 "abc', 'ITEM: Index out of bounds');
+
+  this.assert_error('item 0 456', 'ITEM: Index out of bounds');
+  this.assert_equals('item 1 456', "4");
+  this.assert_equals('item 2 456', "5");
+  this.assert_equals('item 3 456', "6");
+  this.assert_error('item 4 456', 'ITEM: Index out of bounds');
+
+  this.assert_stream('make "a { a b c } ' +
+                     'setitem 2 :a "q ' +
+                     'show :a', '{a q c}\n');
+  this.assert_stream('make "a { a b c }@0 ' +
+                     'setitem 2 :a "q ' +
+                     'show :a', '{a b q}@0\n');
+
+
+  this.assert_error('mditem [0 1] mdarray [1 1]', 'MDITEM: Index out of bounds');
+  this.assert_error('mditem [1 2] mdarray [1 1]', 'MDITEM: Index out of bounds');
+  this.assert_equals('mditem [1 1] mdarray [1 1]', []);
+  this.assert_equals('mditem [0 0] (mdarray [1 1] 0)', []);
+  this.assert_stream('show mditem [1] mdarray [1 1]', '{[]}\n');
+  this.assert_stream('make "a mdarray [ 2 2 ] ' +
+                     'mdsetitem [1 1] :a 1 ' +
+                     'mdsetitem [1 2] :a 2 ' +
+                     'mdsetitem [2 1] :a 3 ' +
+                     'mdsetitem [2 2] :a 4 ' +
+                     'show :a', '{{1 2} {3 4}}\n');
+
+  for (var i = 0; i < 10; i += 1) {
+    this.assert_predicate('pick [ 1 2 3 4 ]', function(x) { return 1 <= x && x <= 4; });
+  }
+  this.assert_equals('remove "b [ a b c ]', ["a", "c"]);
+  this.assert_equals('remove "d [ a b c ]', ["a", "b", "c"]);
+  this.assert_equals('remove "b "abc', 'ac');
+
+  this.assert_equals('remdup [ a b c a b c ]', ["a", "b", "c"]);
+  this.assert_equals('remdup "abcabc', 'abc');
+
+  this.assert_equals('quoted "abc', '"abc');
+  this.assert_equals('quoted [ a b c ]', ['a', 'b', 'c']);
+
+  this.assert_equals('split "a "banana', ['b', 'n', 'n']);
+  this.assert_equals('split "a "alphabetical', ['lph', 'betic', 'l']);
+  this.assert_equals('split 1 [1 2 3 4 1 2 3 4 1 2 3 4 ]', [['2', '3', '4'], ['2', '3', '4'], ['2', '3', '4']]);
+  this.assert_equals('split 2 [1 2 3 4 1 2 3 4 1 2 3 4 ]', [['1'], ['3', '4', '1'], ['3', '4', '1'], ['3', '4']]);
+  this.assert_equals('split 3 [1 2 3 4 1 2 3 4 1 2 3 4 ]', [['1', '2'], ['4', '1', '2'], ['4', '1', '2'], ['4']]);
+  this.assert_equals('split 4 [1 2 3 4 1 2 3 4 1 2 3 4 ]', [['1', '2', '3'], ['1', '2', '3'], ['1', '2', '3']]);
+
+  this.assert_equals('make "s [] repeat 5 [ push "s repcount ] :s', [5, 4, 3, 2, 1]);
+  this.assert_equals('make "s "0 repeat 5 [ push "s repcount ] :s', '543210');
+
+  this.assert_equals('make "s [ a b c ] (list pop "s pop "s pop "s)', ["a", "b", "c"]);
+  this.assert_equals('make "s [ a b c ] pop "s pop "s  :s', ["c"]);
+  this.assert_equals('make "s "abc (list pop "s pop "s pop "s)', ["a", "b", "c"]);
+  this.assert_equals('make "s "abc  pop "s  :s', 'bc');
+
+  this.assert_equals('make "q [] repeat 5 [ queue "q repcount ] :q', [1, 2, 3, 4, 5]);
+  this.assert_equals('make "q "0 repeat 5 [ queue "q repcount ] :q', '012345');
+
+  this.assert_equals('make "q [ a b c ] (list dequeue "q dequeue "q dequeue "q)', ["c", "b", "a"]);
+  this.assert_equals('make "q [ a b c ]  dequeue "q  dequeue "q  :q', ["a"]);
+  this.assert_equals('make "q "abc  (list dequeue "q dequeue "q dequeue "q)', ["c", "b", "a"]);
+  this.assert_equals('make "q "abc  dequeue "q  :q', "ab");
+
+  this.assert_equals('make "a { 1 }  make "b :a  setitem 1 :a 2  item 1 :b', 2);
+  this.assert_error('make "a { 1 }  setitem 1 :a :a', "SETITEM: Can't create circular array");
+  this.assert_error('make "a { 1 }  make "b { 1 }  setitem 1 :b :a  setitem 1 :a :b', "SETITEM: Can't create circular array");
+  this.assert_error('setitem 1 "x 123', 'SETITEM: Expected array');
+
+  this.assert_equals('make "a mdarray [1 1]  make "b :a  mdsetitem [1 1] :a 2  mditem [1 1] :b', 2);
+  this.assert_error('make "a mdarray [1 1]  mdsetitem [1 1] :a :a', "MDSETITEM: Can't create circular array");
+  this.assert_error('mdsetitem [1 1] "x 0', "MDSETITEM: Expected array");
+  this.assert_error('mdsetitem [1 1] {"x} 0', "MDSETITEM: Expected array");
+
+  this.assert_equals('make "a []  .setfirst :a "s  :a', ['s']);
+  this.assert_error('.setfirst "x "y', '.SETFIRST: Expected list');
+
+  this.assert_equals('make "a [a]  .setbf :a [b c]  :a', ['a', 'b', 'c']);
+  this.assert_error('.setbf "x [1]', '.SETBF: Expected non-empty list');
+  this.assert_error('.setbf [] [1]', '.SETBF: Expected non-empty list');
+
+  this.assert_equals('make "a { 1 }  make "b :a  .setitem 1 :a 2  item 1 :b', 2);
+  this.assert_equals('make "a { 1 }  .setitem 1 :a :a  equalp item 1 :a :a', 1);
+  this.assert_error('.setitem 1 "x 123', '.SETITEM: Expected array');
+
+  this.assert_equals('wordp "a', 1);
+  this.assert_equals('wordp 1', 1);
+  this.assert_equals('wordp [ 1 ]', 0);
+  this.assert_equals('wordp { 1 }', 0);
+  this.assert_equals('word? "a', 1);
+  this.assert_equals('word? 1', 1);
+  this.assert_equals('word? [ 1 ]', 0);
+  this.assert_equals('word? { 1 }', 0);
+
+  this.assert_equals('listp "a', 0);
+  this.assert_equals('listp 1', 0);
+  this.assert_equals('listp [ 1 ]', 1);
+  this.assert_equals('listp { 1 }', 0);
+  this.assert_equals('list? "a', 0);
+  this.assert_equals('list? 1', 0);
+  this.assert_equals('list? [ 1 ]', 1);
+  this.assert_equals('list? { 1 }', 0);
+
