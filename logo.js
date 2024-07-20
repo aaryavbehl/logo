31ecd8c66b2ce46e3bef9f6f4e5696d7281042f6
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-20 19:54:30 +0530

    modified:   logo.js

--- logo.js
+++ logo.js
@@ -135,3 +135,131 @@ function LogoInterpreter(turtle, stream, savehook)
       return Promise.resolve();
     }
   }
+  
+  function promiseYieldTime(msec) {
+
+    lastTimeYielded = Date.now() + msec * 2;
+    return new Promise(function(resolve) {
+      setTimeout(resolve, msec);
+    });
+  }
+
+  function to_arity($$func$$, arity) {
+    var parms = [];
+
+    if ($$func$$.length === arity)
+      return $$func$$;
+
+    for (var i = 0; i < arity; ++i)
+      parms.push('a' + i);
+
+    var f = eval('(function ' + $$func$$.name + '(' + parms.join(',') + ')' +
+                 '{ return $$func$$.apply(this, arguments); })');
+    return f;
+  }
+
+  function PRNG(seed) {
+    var S = seed & 0x7fffffff, 
+        A = 48271, 
+        M = 0x7fffffff, 
+        Q = M / A,
+        R = M % A;
+
+    this.next = function PRNG_next() {
+      var hi = S / Q,
+          lo = S % Q,
+          t = A * lo - R * hi;
+      S = (t > 0) ? t : t + M;
+      this.last = S / M;
+      return this.last;
+    };
+    this.seed = function PRNG_seed(x) {
+      S = x & 0x7fffffff;
+    };
+    this.next();
+  }
+
+  function StringMap(case_fold) {
+    this._map = new Map();
+    this._case_fold = case_fold;
+  }
+  Object.defineProperties(StringMap.prototype, {
+    get: {value: function(key) {
+      key = this._case_fold ? String(key).toLowerCase() : String(key);
+      return this._map.get(key);
+    }},
+    set: {value: function(key, value) {
+      key = this._case_fold ? String(key).toLowerCase() : String(key);
+      this._map.set(key, value);
+    }},
+    has: {value: function(key) {
+      key = this._case_fold ? String(key).toLowerCase() : String(key);
+      return this._map.has(key);
+    }},
+    delete: {value: function(key) {
+      key = this._case_fold ? String(key).toLowerCase() : String(key);
+      return this._map.delete(key);
+    }},
+    keys: {value: function() {
+      var keys = [];
+      this._map.forEach(function(value, key) { keys.push(key); });
+      return keys;
+    }},
+    empty: {value: function() {
+      return this._map.size === 0;
+    }},
+    forEach: {value: function(fn) {
+      return this._map.forEach(function(value, key) {
+        fn(key, value);
+      });
+    }}
+  });
+
+  function LogoArray(size, origin) {
+    this._array = [];
+    this._array.length = size;
+    for (var i = 0; i < this._array.length; ++i)
+      this._array[i] = [];
+    this._origin = origin;
+  }
+  LogoArray.from = function(list, origin) {
+    var array = new LogoArray(0, origin);
+    array._array = Array.from(list);
+    return array;
+  };
+  Object.defineProperties(LogoArray.prototype, {
+    item: {value: function(i) {
+      i = Number(i)|0;
+      i -= this._origin;
+      if (i < 0 || i >= this._array.length)
+        throw err("{_PROC_}: Index out of bounds", ERRORS.BAD_INPUT);
+      return this._array[i];
+    }},
+    setItem: {value: function(i, v) {
+      i = Number(i)|0;
+      i -= this._origin;
+      if (i < 0 || i >= this._array.length)
+        throw err("{_PROC_}: Index out of bounds", ERRORS.BAD_INPUT);
+      this._array[i] = v;
+    }},
+    list: {get: function() {
+      return this._array;
+    }},
+    origin: {get: function() {
+      return this._origin;
+    }},
+    length: {get: function() {
+      return this._array.length;
+    }}
+  });
+
+  function Stream(string) {
+    this._string = string;
+    this._index = 0;
+    this._skip();
+  }
+  
+  Object.defineProperties(Stream.prototype, {
+    eof: {get: function() {
+      return this._index >= this._string.length;
+    }},
